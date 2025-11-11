340d47a4381838f59d87ac70812b2483e2b718be
tanmay bansal <121656091+TanmayBansa1@users.noreply.github.com>
2025-11-11 17:59:58 +0530

    forced dark mode, segregated into public route group

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,8 @@
+import HeroSection from "@/components/landing/hero-section";
+export default function Home() {
+  return (
+    <div className="h-screen w-screen">
+      <HeroSection/>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,8 +31,8 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
-          enableSystem
           disableTransitionOnChange
+          forcedTheme="dark"
         >
           {children}
           <Toaster />
